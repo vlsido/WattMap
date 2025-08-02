d67836a859a41719fb940218ad773b4c3fbc1773
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-08-02 14:34:15 +0300

    feat: add LocationView to Map tab

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,39 +1,51 @@
-import { locationList } from "@/LocationList";
+import { Location, locationList } from "@/LocationList";
 import { StyleSheet } from "react-native";
 import {
   SafeAreaView,
   useSafeAreaInsets,
 } from "react-native-safe-area-context";
 import MapView, { Marker } from "react-native-maps";
+import LocationView from "@/components/map/LocationView";
+import { useState } from "react";
+import { useBottomTabOverflow } from "@/components/ui/TabBarBackground";
 
 export default function Index() {
   const insets = useSafeAreaInsets();
 
+  const [selectedLocation, setSelectedLocation] = useState<Location | null>(
+    null,
+  );
+
+  const bottom = useBottomTabOverflow();
+
   return (
-    <>
-      <SafeAreaView style={{ flex: 1 }}>
-        <MapView
-          style={[StyleSheet.absoluteFill, { top: insets.top }]}
-          initialRegion={{
-            latitude: 59.441466,
-            longitude: 24.751405,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        >
-          {locationList.map((location, index) => (
-            <Marker
-              key={index}
-              coordinate={location.point}
-              title={location.name}
-              description={location.address}
-              image={require("../../assets/images/marker.png")}
-              onSelect={() => console.log("select")}
-              onDeselect={() => console.log("deselect")}
-            />
-          ))}
-        </MapView>
-      </SafeAreaView>
-    </>
+    <SafeAreaView
+      style={[StyleSheet.absoluteFill, { top: insets.top, bottom }]}
+    >
+      <MapView
+        style={[StyleSheet.absoluteFill]}
+        initialRegion={{
+          latitude: 59.441466,
+          longitude: 24.751405,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        toolbarEnabled={false}
+        loadingEnabled={true}
+      >
+        {locationList.map((location, index) => (
+          <Marker
+            key={index}
+            coordinate={location.point}
+            title={location.name}
+            description={location.address}
+            image={require("../../assets/images/marker.png")}
+            onSelect={() => setSelectedLocation(location)}
+            onDeselect={() => setSelectedLocation(null)}
+          />
+        ))}
+      </MapView>
+      <LocationView location={selectedLocation} />
+    </SafeAreaView>
   );
 }
