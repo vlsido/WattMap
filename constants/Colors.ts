6cf6c0ac504b5d8ec2032f236065cc0ce31be34c
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-08-02 14:26:16 +0300

    feat: add colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -9,18 +9,24 @@ const tintColorDark = "#fff";
 export const Colors = {
   light: {
     text: "#11181C",
+    warnText: "#1256A4",
     background: "#fff",
+    border: "#151718",
     tint: tintColorLight,
     icon: "#687076",
     tabIconDefault: "#687076",
     tabIconSelected: tintColorLight,
+    primaryGreen: "#14AE5C",
   },
   dark: {
     text: "#ECEDEE",
+    warnText: "#FDFD96",
     background: "#151718",
+    border: "#fff",
     tint: tintColorDark,
     icon: "#9BA1A6",
     tabIconDefault: "#9BA1A6",
     tabIconSelected: tintColorDark,
+    primaryGreen: "#1EE78D",
   },
 };
