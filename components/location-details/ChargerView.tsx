6dc179ab4e9f3a0f9c4e3c4054bf0d19701596d4
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-08-07 01:46:14 +0300

    feat: add ChargerView and ConnectorView components

---
+++ components/location-details/ChargerView.tsx
@@ -0,0 +1,56 @@
+import { StyleSheet, View } from "react-native";
+import { ThemedText } from "../ThemedText";
+import ConnectorView from "./ConnectorView";
+import { Connector } from "@/types/common";
+
+interface ChargerProps {
+  id: string;
+  connectors: Connector[];
+  onSelectConnector: (connector: Connector) => void;
+}
+
+function ChargerView(props: ChargerProps) {
+  return (
+    <View style={[styles.gap, styles.charger]}>
+      <View style={[styles.row, { justifyContent: "space-between" }]}>
+        <ThemedText>Charger ID:</ThemedText>
+        <ThemedText>{props.id}</ThemedText>
+      </View>
+      {props.connectors
+        .sort((a, b) => {
+          if (a.status === "AVAILABLE" && b.status !== "AVAILABLE") return -1;
+          if (a.status !== "AVAILABLE" && b.status === "AVAILABLE") return 1;
+          return 0;
+        })
+        .map((connector) => (
+          <ConnectorView
+            key={connector.id}
+            id={connector.id}
+            connectorType={connector.connectorType}
+            status={connector.status}
+            priceInCentsPerKWh={connector.priceInCentsPerKWh}
+            maxPowerOutputKW={connector.maxPowerOutputKW}
+            onSelect={props.onSelectConnector}
+          />
+        ))}
+    </View>
+  );
+}
+
+export default ChargerView;
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  gap: {
+    gap: 10,
+  },
+  charger: {
+    backgroundColor: "#63937790",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderRadius: 30,
+  },
+});
---
+++ components/location-details/ConnectorView.tsx
@@ -0,0 +1,128 @@
+import Animated, { CSSAnimationKeyframes } from "react-native-reanimated";
+import { StyleSheet, View } from "react-native";
+import { ThemedText } from "../ThemedText";
+import ConnectorIcon from "../connector/ConnectorIcon";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { IconSymbol } from "../ui/IconSymbol";
+import { ThemedPressable } from "../ThemedPressable";
+import { Connector, ConnectorStatus, ConnectorType } from "@/types/common";
+
+interface ConnectorProps {
+  id: number;
+  connectorType: ConnectorType;
+  status: ConnectorStatus;
+  priceInCentsPerKWh: number;
+  maxPowerOutputKW: number;
+  onSelect: (connector: Connector) => void;
+}
+
+function ConnectorView(props: ConnectorProps) {
+  let statusColor = "#1EE78D";
+
+  if (props.status === "IN USE") {
+    statusColor = "#B0A7FF";
+  } else if (props.status === "UNAVAILABLE") {
+    statusColor = "#ED060C";
+  }
+
+  const ping: CSSAnimationKeyframes = {
+    from: {
+      transform: [{ scale: 1 }],
+      opacity: 1,
+    },
+    to: {
+      transform: [{ scale: 0.9 }],
+      opacity: 0.5,
+    },
+  };
+
+  return (
+    <ThemedPressable
+      style={[
+        styles.connector,
+        styles.gap,
+        props.status !== "AVAILABLE" && { opacity: 0.75 },
+      ]}
+      onPress={() =>
+        props.onSelect({
+          id: props.id,
+          connectorType: props.connectorType,
+          status: props.status,
+          priceInCentsPerKWh: props.priceInCentsPerKWh,
+          maxPowerOutputKW: props.maxPowerOutputKW,
+        })
+      }
+    >
+      <View>
+        <View style={[styles.row, styles.gap]}>
+          <View
+            style={{
+              height: 24,
+              width: 24,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Animated.View
+              style={[
+                styles.ellipse,
+                {
+                  backgroundColor: statusColor,
+                  animationName: ping,
+                  animationDuration: "1s",
+                  animationIterationCount: "infinite",
+                  animationTimingFunction: "ease-in-out",
+                  animationDirection: "alternate",
+                  animationPlayState:
+                    props.status === "AVAILABLE" ? "running" : "paused",
+                },
+              ]}
+            />
+          </View>
+          <ThemedText type="small">{props.status}</ThemedText>
+        </View>
+      </View>
+      <View style={[styles.row, styles.gap]}>
+        <ConnectorIcon
+          type={props.connectorType}
+          status={props.status}
+          size={24}
+        />
+        <ThemedText type="defaultSemiBold">{props.connectorType}</ThemedText>
+      </View>
+      <View style={[styles.row, styles.gap]}>
+        <MaterialCommunityIcons
+          name="lightning-bolt"
+          color={"#FFCC00"}
+          size={24}
+        />
+        <ThemedText>Up to {props.maxPowerOutputKW}kW</ThemedText>
+      </View>
+      <View style={[styles.row, styles.gap]}>
+        <IconSymbol name="banknote.fill" size={24} color={"white"} />
+        <ThemedText>€{props.priceInCentsPerKWh / 100}/kWh</ThemedText>
+      </View>
+    </ThemedPressable>
+  );
+}
+
+export default ConnectorView;
+
+const styles = StyleSheet.create({
+  connector: {
+    padding: 10,
+    borderRadius: 20,
+  },
+  ellipse: {
+    height: 7,
+    width: 7,
+    borderRadius: 60,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  gap: {
+    gap: 10,
+  },
+});
