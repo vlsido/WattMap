6a32f5708fce66f89529037814717871fcd780b1
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-08-07 22:44:45 +0300

    chore: configure testing eslint plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import tseslint from "typescript-eslint";
 import react from "eslint-plugin-react";
 import pluginQuery from "@tanstack/eslint-plugin-query";
 import eslintConfigPrettierFlat from "eslint-config-prettier/flat";
+import testingLibrary from "eslint-plugin-testing-library";
 
 export default [
   ...pluginQuery.configs["flat/recommended"],
@@ -31,6 +32,15 @@ export default [
       "no-unreachable-loop": ["warn"],
       "no-invalid-regexp": ["error"],
       "react/react-in-jsx-scope": "off",
+    },
+  },
+  {
+    files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+    plugins: {
+      "testing-library": testingLibrary,
+    },
+    rules: {
+      ...testingLibrary.configs.react.rules,
     },
   },
 ];
