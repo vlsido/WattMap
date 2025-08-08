bcc1ef32fa5d3551dac67fe548345f6957237892
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-08-08 15:24:35 +0300

    feat: add new Animated component

--- constants/AnimatedComponents.ts
+++ constants/AnimatedComponents.ts
@@ -1,3 +1,6 @@
+import { ThemedPressable } from "@/components/ui/themed/ThemedPressable";
+import { ThemedText } from "@/components/ui/themed/ThemedText";
+import { ThemedView } from "@/components/ui/themed/ThemedView";
 import { LinearGradient } from "expo-linear-gradient";
 import { Pressable } from "react-native";
 import Animated from "react-native-reanimated";
@@ -6,3 +9,10 @@ export const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 
 export const AnimatedLinearGradient =
   Animated.createAnimatedComponent(LinearGradient);
+
+export const AnimatedThemedView = Animated.createAnimatedComponent(ThemedView);
+
+export const AnimatedThemedText = Animated.createAnimatedComponent(ThemedText);
+
+export const AnimatedThemedPressable =
+  Animated.createAnimatedComponent(ThemedPressable);
